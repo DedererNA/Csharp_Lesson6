3dd75f51d4aba04c775bf345e58a1d90a58c7940
Nikolay Dederer <cmrddrr@gmail.com>
2023-09-24 15:09:25 +0300

    task41 finished

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] CustomArr (int size)
+{
+    Console.WriteLine($"Введите {size} чисел.");
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        Console.WriteLine($"Введите {i+1}-е число:");
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int PosCount (int[] array)
+{
+    int count=0;
+    for (int i=0; i<array.Length; i++)
+    {
+        if (array[i]>=0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int m = 6;
+int[] array=CustomArr(m);
+Console.WriteLine(string.Join(", ", array));
+Console.WriteLine($"Количество положительных чисел: {PosCount(array)}");
